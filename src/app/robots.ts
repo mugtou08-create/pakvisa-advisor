9b28bfac024c7feb3e1b71e512df4222c02831bb
Z User <z@container>
2026-08-23 23:56:20 +0000

    Add robots.txt with sitemap reference for Google SEO

---
+++ src/app/robots.ts
@@ -0,0 +1,13 @@
+import type { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    sitemap: 'https://pakvisaadvisor.com/sitemap.xml',
+  };
+}
